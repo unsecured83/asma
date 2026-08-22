87102ba6e9c1f610b9bde3cadc1dbc5af0619a42
unsecured83 <165594437+unsecured83@users.noreply.github.com>
2026-08-22 07:25:27 +0700

    Update Supabase credentials

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // Konfigurasi Kredensial Supabase
-const SUPABASE_URL = 'https://ydxffmteemrtaeenlaal.supabase.co'; 
-const SUPABASE_ANON_KEY = 'sb_publishable_4u5SvTvyQN4Yi04A1xIQVA_XFdbryHW'; 
+const SUPABASE_URL = 'https://hgbgghngbvxyqwugwhrw.supabase.co'; 
+const SUPABASE_ANON_KEY = 'sb_publishable_31-0APFdEWMH3ob-Lm0nZA_NfAxssDX';
 
 // Inisialisasi Supabase Client (Standardisasi untuk semua halaman)
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
